6540c6ba967362e962a78dd5fdb03ffd0c5f5f43
KaioCBraga <oficial_kaio@hotmail.com>
2026-02-19 23:05:50 -0300

    add: implement reservation system with validation and exception handling

---
+++ src/s14Exceptions/Aulas/Aula005/model/exceptions/DomainException.java
@@ -0,0 +1,8 @@
+package s14Exceptions.Aulas.Aula005.model.exceptions;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String msg) {
+        super(msg);
+    }
+
+}
